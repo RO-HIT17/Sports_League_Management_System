83acf83db25a5de48168d172066aaf66868a2025
vjk-2k5 <svijaykrishna12@gmail.com>
2024-11-10 00:48:19 +0530

    desp update

--- client/config/site.ts
+++ client/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Starter Template",
-  description: "A starter template for Next.js created by code crusaeders",
+  name: "Sports League System",
+  description: "Sports League System",
   navItems: [
     {
       label: "Home",
